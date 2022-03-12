eebcb00393000fc4d1f34c4751bbc6a3527c40dc
Shrikant Dhayje <35664193+shriekdj@users.noreply.github.com>
2022-03-12 17:40:31 +0530

    Create variables_and_gifs.py

---
+++ variables_and_gifs.py
@@ -0,0 +1,121 @@
+carryminati_words = ["carry", "rupali", "bantai", "charas", "ganja", "welcome"]
+
+starter_words = ["Cheer up!", "Hang in there.", "You are agreat person / bot"]
+
+yo_words = ["yo yo", "bantai", "charas", "talont", "rupali", "ganja", "talent", "tolant", "yo yo"]
+
+yo_gifs = [
+    "https://tenor.com/view/charas-ganja-merko-pyara-ganja-carry-minati-rap-swag-gif-17366017",
+    "https://tenor.com/view/ae-rupali-yo-yo-bantai-rapper-carryminati-a-silver-ajey-nagar-gif-17365936",
+    "https://tenor.com/view/carryminati-rap-yoyo-bantai-gif-19361295"
+]
+
+flirt_words = [
+    "insta", "fb", "facebook", "number", "no.", "single", "boyfriend",
+    "girlfriend", "bf", "gf", "umar", "umr", "whatsapp", "umar"
+]
+
+flirt_gifs = [
+    "https://tenor.com/view/carry-minati-carry-minati-roast-ajeyn-agar-carry-ajey-gif-17327702",
+    "https://tenor.com/view/bahut-tez-bahut-tez-bahut-tez-ho-rahe-ho-beta-bahut-tez-gif-17239646",
+    "https://tenor.com/view/carryminati-ajey-nagar-gif-18342995"
+]
+
+welcomes_gifs = [
+    "Toh Kaise Hai Aap Log",
+    "https://tenor.com/view/carry-minati-toh-kaise-hai-aap-log-how-are-you-gif-17790905",
+    "https://tenor.com/view/to-kaise-hai-aap-log-so-how-are-you-guys-eyeglasses-how-are-you-gif-17388135",
+    "https://tenor.com/view/carry-minati-ajey-nagar-indian-you-tuber-carry-minati-roast-carry-gif-17685028"
+]
+
+huh_words = ["huh", "kehna kya", "kahna kya"]
+
+huh_gifs = [
+    "https://tenor.com/view/carryminati-ajey-nagar-indian-you-tuber-carryminati-roast-carry-gif-17582217",
+    "https://tenor.com/view/nick-young-question-marks-what-excuse-me-huh-gif-4486363",
+    "https://tenor.com/view/wtf-is-going-on-wtf-what-do-you-mean-confused-huh-gif-16986940",
+    "https://tenor.com/view/head-scratcher-confused-perplexed-huh-clash-with-cam-gif-10198268"
+]
+
+kuchbhi_words = ["kuch bhi", "kuchh bhi", "kuchh vi", "kuchh vi"]
+
+kuchbhi_gifs = [
+    "https://tenor.com/view/kuchh-bhi-arnab-goswami-kuchh-bhi-anything-upset-gif-17125706",
+    "https://tenor.com/view/kuch-bhi-bol-sakte-hai-%E0%A4%95%E0%A5%81%E0%A4%9B%E0%A4%AD%E0%A5%80%E0%A4%AC%E0%A5%8B%E0%A4%B2%E0%A4%B8%E0%A4%A4%E0%A5%87%E0%A4%B9%E0%A5%88-%E0%A4%9C%E0%A5%8B%E0%A4%AE%E0%A4%B0%E0%A5%8D%E0%A4%9C%E0%A4%BC%E0%A5%80%E0%A4%AC%E0%A5%8B%E0%A4%B2%E0%A4%A8%E0%A4%BE-%E0%A4%AA%E0%A4%B0%E0%A5%87%E0%A4%B6%E0%A4%BE%E0%A4%A8-can-talk-anything-gif-17376962",
+    "https://tenor.com/view/kuch-bhi-yaar-ranveer-singh-vaani-kapoor-pinkvilla-%E0%A4%95%E0%A5%81%E0%A4%9B%E0%A4%AD%E0%A5%80%E0%A4%AF%E0%A4%BE%E0%A4%B0-gif-19192270",
+    "https://tenor.com/view/kha-bollywood-deepika-padukone-gif-5654250",
+    "Are Yaar Nashay Kam",
+    "Kuch Bhi Yaar Matlab Kuch Bhi"
+]
+
+gm_words = ["good morning", "gm", "goodmorning", "guten morgen"]
+
+gm_gifs = [
+    "https://tenor.com/view/good-morning-prank-good-day-gif-8572829",
+    "https://tenor.com/view/chicken-funny-hey-hi-hello-gif-4904696",
+    "https://tenor.com/view/alegria-okay-good-morning-good-morning-gif-13172443",
+    "goodMorning Tumhe Bhi",
+    "goodMorning Jaag Gaye",
+    "https://tenor.com/view/subah-morning-gm-savera-suprabhat-gif-12450617",
+    "https://tenor.com/view/carryminati-ajey-nagar-indian-you-tuber-carryminati-roast-carry-gif-17761015"
+]
+
+gnoon_words = ["good afternoon", "goodafternoon"]
+
+gnoon_gifs = [
+    "goodAfternoon buddy",
+    "https://tenor.com/view/tigger-happy-godd-morning-good-afternoon-gif-12447284",
+    "Kal Milte Hai goodNight",
+    "https://tenor.com/view/yes-night-yawn-sleepy-gif-10578126"
+]
+
+gn_words = ["good night", "gn", "goodnight"]
+
+gn_gifs = [
+    "Thanks for asking goodNight",
+    "Tumhe Bhi goodNight",
+    "Kal Milte Hai goodNight",
+    "https://tenor.com/view/yes-night-yawn-sleepy-gif-10578126"
+]
+
+
+bruh_words = ["bruh"]
+
+bruh_gifs = [
+    "https://tenor.com/view/confused-what-nigga-please-really-loop-gif-4966361",
+    "https://tenor.com/view/shocked-ohreally-flabbergasted-astonished-gif-7877609",
+    "https://cdn.discordapp.com/attachments/803210545284382804/803227219879133204/unknown.gif",
+    "https://cdn.discordapp.com/attachments/803210545284382804/803227298949103626/unknown.gif"
+]
+
+natsu_words = ["natsu","natsu dragneel"]
+
+natsu_gifs = [
+    "https://cdn.discordapp.com/attachments/803210545284382804/803228581962317834/Untitled_1_480p_5.gif"
+]
+
+control_words = ["control","controll""control","controll"]
+
+control_gifs = [
+    "https://tenor.com/view/welcome-control-uday-nana-patekar-control-control-gif-17756424",
+    "https://tenor.com/view/welcome-control-control-uday-nana-patekar-gif-17756711"
+]
+
+takleef_words = ["taklif hua","takleef hua", "bichare ko", "bichaare ko", "bechare ko", "bechaare ko"]
+
+takleef_gifs = [
+    "https://tenor.com/view/welcome-bolne-de-bolne-de-takleef-hua-hai-bechare-ko-anil-kapoor-gif-17756593",
+    "https://tenor.com/view/welcome-bolne-de-takleef-hua-bechare-ko-anil-kapoor-funny-gif-16779282"
+]
+
+nikal_words = ["nikal", "nikal lawfe"]
+
+nikal_gifs = [
+    "https://tenor.com/view/nikal-laude-nikal-lavde-fursat-laude-pehli-fursat-gif-14527278"
+]
+
+nahi_words = ["nahi", "nai"]
+
+nahi_gifs = [
+    "https://tenor.com/view/no-nah-nahi-sanjay-dutt-gif-15456089"
+]
